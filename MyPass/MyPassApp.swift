6217bef0963971f5e09eb8ff2d4f35884f287839
leonardomsilva <leo_marquesilva@hotmail.com>
2023-10-13 17:04:05 -0300

    Initial Commit

---
+++ MyPass/MyPassApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyPassApp.swift
+//  MyPass
+//
+//  Created by Leonardo Marques on 13/10/23.
+//
+
+import SwiftUI
+
+@main
+struct MyPassApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
